8f3dfbfe5b81b3373ca0dd52482f16842bf3c496
John F Hoeppner <johnh.abbykus@gmail.com>
2022-03-09 10:46:46 +0700

    Update blink.py

--- Examples/MicroPython/blink.py
+++ Examples/MicroPython/blink.py
@@ -1,6 +1,6 @@
 import machine
 import time
-pin = machine.Pin(2, machine.Pin.OUT)  # GPIO 2 is the ESP-12F LED
+pin = machine.Pin(9, machine.Pin.OUT)  # GPIO 9 is the LED on QDEV-ESP32-C3
 for i in range(30):
    pin.off()
    time.sleep(0.5)
